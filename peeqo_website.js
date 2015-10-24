c66504f1032b840055181ddcf14dea3287edb3c9
shekit <as7790@nyu.edu>
2015-10-24 18:36:40 -0400

    added basic authentication

--- peeqo_website.js
+++ peeqo_website.js
@@ -6,13 +6,13 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
+  Template.home.helpers({
     "emails": function(){
       return Emails.find({}); 
     }
   })
 
-  Template.hello.events({
+  Template.home.events({
     'click button': function () {
       // increment the counter when button is clicked
       Meteor.call("saveEmail", "abhishek", function(err, result){
@@ -26,6 +26,15 @@ if (Meteor.isServer) {
 
   Meteor.startup(function(){
     process.env.MAIL_URL = 'smtp://postmaster@sandboxb179e336a1664590b1279d4409dbb3e5.mailgun.org:1ac9fd9f708c626dd29bf27bcfe42932@smtp.mailgun.org:587'
+  
+    if(Meteor.users.find().count() == 0){
+      Accounts.createUser({
+        email: 'a@2.com',
+        password: '123456'
+      })
+    } else {
+      console.log("admin already assigned")
+    }
   })
 
   Meteor.methods({
@@ -53,3 +62,48 @@ if (Meteor.isServer) {
 
   })
 }
+
+Router.configure({
+  'layoutTemplate': 'main'
+})
+
+Router.onBeforeAction(function(){
+  var user = Meteor.user();
+
+  if(Meteor.user()){
+    if(user['emails'][0]['address'] != 'a@2.com'){
+      this.render('home');
+    } else {
+      this.next();
+    }
+  } else {
+    this.next();
+  }
+
+}, {
+  only: ['admin']
+});
+
+Router.route('/',{
+  name: "home",
+  template: "home"
+})
+
+Router.route('/login',{
+  name: "login",
+  template: "login"
+})
+
+Router.route('/nimda', {
+  name:'admin',
+  template: 'admin',
+  // onBeforeAction: function(){
+  //   var user = Meteor.user();
+
+  //   if(user['emails'][0]['address'] != 'a@2.com'){
+  //     this.render('home')
+  //   } else {
+  //     this.next();
+  //   }
+  // }
+})
